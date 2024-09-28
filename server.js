692b93333c8e74eb981169b80d30521d2d57cdb5
David MARTINS <david.joaquim.martins@gmail.com>
2024-09-28 03:15:50 +0200

    new url

--- server.js
+++ server.js
@@ -122,8 +122,11 @@ app.use(express.urlencoded({ extended: true }));
 const connectionString =
 //   "mongodb://" + process.env.mongodb_host + ":" + process.env.mongodb_port;
 // "mongodb+srv://davidjoaquimmartins:david@clusterd.gllspzx.mongodb.net/"
-`mongodb+srv://davidjoaquimartins:david@clusterd.gllspzx.mongodb.net/?retryWrites=true&w=majority&appName=ClusterD`
-`mongodb+srv://davidjoaquimartins:david@cluster0.mongodb.net/JeuxCartes?retryWrites=true&w=majority`
+// `mongodb+srv://davidjoaquimartins:david@clusterd.gllspzx.mongodb.net/?retryWrites=true&w=majority&appName=ClusterD`
+// `mongodb+srv://davidjoaquimartins:david@cluster0.mongodb.net/JeuxCartes?retryWrites=true&w=majority`
+`mongodb+srv://davidjoaquimmartins:david@clusterd.gllspzx.mongodb.net/?retryWrites=true&w=majority&appName=ClusterD`
+
+
 
 const client = new MongoClient(connectionString);
 const dbName = process.env.mongodb_dbname;
